b02297739684cc0d4a1f04e64283a8a26aff0889
Vasiliy Gorilev <89159793029@ya.ru>
2024-04-18 13:01:26 +0300

    делаю ДЗ к семинару 6 продвинутого JS

--- 02_HTML_CSS/01_DZ/scripts/scripts.js
+++ 02_HTML_CSS/01_DZ/scripts/scripts.js
@@ -1,7 +1,7 @@
 const productEls = JSON.parse(catalogData);
 const productEl = document.querySelector("div.products_elements");
 
-productEls.forEach((elem) => {
+/* productEls.forEach((elem) => {
   const cardEl = document.createElement("div");
   cardEl.classList.add("products_card");
   productEl.appendChild(cardEl);
@@ -48,4 +48,38 @@ productEls.forEach((elem) => {
   if (elem.id === 3) {
     cardEl.classList.add("products__hide");
   }
+}); */
+
+function displayList(arrData, rowPerPage, page) {
+  const postsEl = document.querySelector("");
+
+  const start = rowPerPage * page;
+  const end = start + rowPerPage;
+  const paginatedData = arrData.slice(start, end);
+
+  paginatedData.forEach((el) => {
+    const postEl = document.createElement("div");
+    postEl.classList.add("");
+    postEl.innerHTML = el;
+    postsEl.appendChild(postEl);
+  });
+}
+
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  
+}
+productEls.forEach((el) => {
+  const tempEl = document.getElementById("cardTemplate");
+
+  const clone = tempEl.content.cloneNode(true);
+  console.log(clone);
+  clone.querySelector("img.products_card-img").src = el.srcImg;
+  clone.querySelector("img.products_card-img").alt = el.imgAlt;
+  clone.querySelector("a.products_card_img-button").href = el.hrefCard;
+  clone.querySelector("p.products_card_down_header").innerHTML = el.productName;
+  clone.querySelector("p.products_card_down_text").innerHTML = el.productText;
+  clone.querySelector("p.products_card_down_price").innerHTML = el.productPrice;
+
+  productEl.appendChild(clone);
 });
